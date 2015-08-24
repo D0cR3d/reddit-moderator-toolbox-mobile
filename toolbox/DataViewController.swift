c5419c241f88fc9b59b0aa2eb10bb7ab96955f42
D0cR3d <d0cr3d@neonet.me>
2015-08-23 21:25:12 -0500

    Initial Commit

---
+++ toolbox/DataViewController.swift
@@ -0,0 +1,38 @@
+//
+//  DataViewController.swift
+//  toolbox
+//
+//  Created by Mike Wohlrab on 8/23/15.
+//  Copyright (c) 2015 Layer 7 Solutions. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: AnyObject?
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        if let obj: AnyObject = dataObject {
+            self.dataLabel!.text = obj.description
+        } else {
+            self.dataLabel!.text = ""
+        }
+    }
+
+
+}
+
